ae856c8a3a9692220cd4c39f6e0a262f6c12b442
Austin Wasson <austinpwasson@gmail.com>
2024-01-08 00:23:53 -0700

    test models

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -16,6 +16,10 @@ module Dummy
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.after_initialize do
+      Rails.application.eager_load!
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
